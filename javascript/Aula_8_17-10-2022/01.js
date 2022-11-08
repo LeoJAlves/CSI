79ccbfc0b510d89f7331ec94066ed81f32f9d78a
LeoJAlves <87732893+LeoJAlves@users.noreply.github.com>
2022-11-08 18:39:13 -0300

    Create 01.js

---
+++ javascript/Aula_8_17-10-2022/01.js
@@ -0,0 +1,35 @@
+ <script>
+
+/*
+ATIVIDADE PRÁTICA 1
+Crie um programa em JavaScript que leia o nome e a média de
+um aluno, apresente a mensagem para o aluno:
+“Parabéns...Você foi aprovado(a)!” ou, então, “Ops...Você foi
+reprovado(a)!”. A situação de aprovado ou reprovado é
+definida pela média das notas, que deve ser 7.0 ou superior
+para aprovação.
+*/
+
+
+var nome = prompt("Digite o nome do aluno:");
+
+var media = prompt("Digite a média do aluno:");
+
+if(media >=7){
+
+alert("Parabéns...Você foi aprovado(a)!");
+
+}else{
+
+   alert("Ops...Você foi reprovado(a)!");
+
+}
+
+
+
+
+
+
+
+
+  </script>
